da091b00d9f9d06efb5f86090d28f0f52bbbf4e3
jnguyen164 <jennyn46@icloud.com>
2026-01-04 01:02:46 -0500

    Fix CORS issue

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:3001', // frontend port
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
